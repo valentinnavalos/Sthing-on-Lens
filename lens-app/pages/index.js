c88a9cafce7460c95ec13d94ee1191fdc4ec31da
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-08-18 16:29:27 -0300

    feat: fetch popular profiles from lens

--- lens-app/pages/index.js
+++ lens-app/pages/index.js
@@ -1,3 +1,19 @@
+import { useState, useEffect } from "react";
+import { client, recommendProfiles } from "../api";
+
 export default function Home() {
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
+
+  const fetchProfiles = async () => {
+    try {
+      const response = await client.query(recommendProfiles).toPromise();
+      console.log({ response });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
   return <div></div>;
 }
