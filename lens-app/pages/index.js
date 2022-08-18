22f2758acc1254d3660e79c4cf1482404d603bd4
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-08-18 16:39:56 -0300

    feat: store the fetch response at a local state

--- lens-app/pages/index.js
+++ lens-app/pages/index.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { client, recommendProfiles } from "../api";
 
 export default function Home() {
+  const [profiles, setProfiles] = useState([]);
+
   useEffect(() => {
     fetchProfiles();
   }, []);
@@ -9,6 +11,7 @@ export default function Home() {
   const fetchProfiles = async () => {
     try {
       const response = await client.query(recommendProfiles).toPromise();
+      setProfiles(response.data.recommendProfiles);
       console.log({ response });
     } catch (error) {
       console.log({ error });
